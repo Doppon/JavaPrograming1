4992fe9aae6387a21711599e3ab36b9b4a061837
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-16 10:29:47 +0900

    リファクタリング

--- Lesson1/SampleSrc/SampleClass02.java
+++ Lesson1/SampleSrc/SampleClass02.java
@@ -1,18 +1,18 @@
-class Account{
-  public int amt = 1000;
-  public void save(int j){
-    amt = amt + j;
-  }
-  public int show(){
-    return amt;
-  }
+class Account {
+	public int amt = 1000;
+	public void save(int j) {
+    	amt = amt + j;
+  	}
+	public int show() {
+		return amt;
+	}
 }
 
-class SampleClass02{
-  public static void main(String[] args){
-    Account myAccount = new Account();
-    myAccount.save(2000);
-    int j = myAccount.show();
-    System.out.println("預金："+j);
-  }
+class SampleClass02 {
+	public static void main(String[] args) {
+		Account myAccount = new Account();
+		myAccount.save(2000);
+		int j = myAccount.show();
+		System.out.println("預金："+j);
+	}
 }
