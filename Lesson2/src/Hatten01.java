441cc9365ad14a5b609585b0355cfbc73365ea02
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-16 13:21:01 +0900

    コマンドライン引数の問題を追加

---
+++ Lesson2/src/Hatten01.java
@@ -0,0 +1,9 @@
+class Hatten01 {
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			System.out.println(args[0]);
+		} else {
+			System.out.println("引数はひとつだけです。");
+		}
+	}
+}
