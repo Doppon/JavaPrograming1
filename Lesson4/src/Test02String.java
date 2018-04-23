9e2ded80711e9be05d4218eb769e2fcd8cb4daed
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-23 10:56:25 +0900

    それぞれの演習を追加

--- Lesson4/src/Test02String.java
+++ Lesson4/src/Test02String.java
@@ -13,6 +13,18 @@ class Test02A {
     Dog akita = new Dog();
     akita.setName("HACHI-KOU");
     akita.getName();
+
+    // 文字列の長さの出力
     System.out.println("文字列の長さ：" + akita.name.length());
+
+    // 小文字にして出力
+    System.out.println(akita.name.toLowerCase());
+
+    // HA -> PO
+    System.out.println(akita.name.replace("HA","PO"));
+
+    // ハイフンより前を出力
+    String[] array = akita.name.split("-");
+    System.out.println(array[0]);    
   }
 }
