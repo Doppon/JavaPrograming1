17e15cd217b12b6ff638fb7caeea06a05b92bb47
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-23 11:19:02 +0900

    10000 -> 50000

--- Lesson4/src/Test02StringBuffer.java
+++ Lesson4/src/Test02StringBuffer.java
@@ -2,14 +2,14 @@ class Test02B {
   public static void main(String[] args) {
     // ゴリ押しの処理
     String str = "AAA";
-    for(int i = 0; i < 10000; i++) {
+    for(int i = 0; i < 50000; i++) {
       str = str + "BCDEF";
     }
     System.out.println("end_string");
     
     // StringBefferを用いた処理
     StringBuffer strBuff = new StringBuffer("AAA");
-    for(int i = 0; i < 10000; i++) {
+    for(int i = 0; i < 50000; i++) {
       strBuff.append("BCDEF");
     }
     String str_buff = strBuff.toString();
