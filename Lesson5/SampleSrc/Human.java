6c570eb1eb269548c93f23cbc6392a9751ac8396
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-07 09:48:52 +0900

    デフォルト値を設定

--- Lesson5/SampleSrc/Human.java
+++ Lesson5/SampleSrc/Human.java
@@ -3,6 +3,12 @@ public class Human {
   int birthday;
   int manpukudo;
 
+  Human(){
+    this.name = "hogemaru";
+    this.birthday = 19990909;
+    this.manpukudo = 50;
+  }
+
   void eat() {
     this.manpukudo += 60;
   }
