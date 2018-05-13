7090be1b58355d5575a8651cc33b8968d3f9d06f
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-13 10:55:03 +0900

    Dog3クラスを追加

---
+++ Lesson6/src/Dog3.java
@@ -0,0 +1,16 @@
+class Dog3 {
+  Dog3(){
+    // 引数なしのコンストラクタ
+  }
+  Dog3(String s){
+    // Stringクラスのオブジェクトsを引数に
+    // 持つコンストラクタ
+  }
+  String name="HACHI";
+  void setName(String s){
+    name = s;
+  }
+  String whoami(){
+    return name;
+  }
+}
