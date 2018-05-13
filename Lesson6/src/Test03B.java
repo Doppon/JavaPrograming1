ab93f845bbfa651440b9eae18d071d0bbeb8e11f
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-14 01:25:28 +0900

    重複されている文を削除

--- Lesson6/src/Test03B.java
+++ Lesson6/src/Test03B.java
@@ -3,7 +3,6 @@ class Test03B{
     Dog3 sakura = new Dog3("SAKURA");
     Dog3 momo = new Dog3("MOMO");
     sakura.numOfLegs = 10;
-    System.out.println(momo.numOfLegs);
 
     System.out.println(momo.numOfLegs);
     System.out.println(Dog3.showNumLegs());
