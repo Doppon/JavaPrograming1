14fafd50253b3c6e36b1549c0d9efb9040841bd8
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-14 01:17:19 +0900

    Test03Bクラスを追加

---
+++ Lesson6/src/Test03B.java
@@ -0,0 +1,11 @@
+class Test03B{
+  public static void main(String[] args){
+    Dog3 sakura = new Dog3("SAKURA");
+    Dog3 momo = new Dog3("MOMO");
+    sakura.numOfLegs = 10;
+    System.out.println(momo.numOfLegs);
+
+    System.out.println(memo.numOfLegs);
+    System.out.println(Dog3.showNumLegs());
+  }
+}
