6b5505b3e5c1b8cf6b46fe313ddf002daea6112e
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-13 11:51:14 +0900

    numOfLegsクラス変数を追加

--- Lesson6/src/Dog3.java
+++ Lesson6/src/Dog3.java
@@ -2,10 +2,12 @@ class Dog3 {
   Dog3(){
     // 引数なしのコンストラクタ
   }
+
   Dog3(String s){
     // Stringクラスのオブジェクトsを引数に
     // 持つコンストラクタ
   }
+  
   String name="HACHI";
   void setName(String s){
     name = s;
@@ -13,4 +15,7 @@ class Dog3 {
   String whoami(){
     return name;
   }
+
+  static int numOfLegs = 4;
+  
 }
