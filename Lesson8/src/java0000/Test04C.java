ec0e532236ffcc60528bc411b27922d777f11540
Doppon <zaxpkuraudo7777@gmail.com>
2018-05-14 11:20:33 +0900

    ファイル名を変更

---
+++ Lesson8/src/java0000/Test04C.java
@@ -0,0 +1,9 @@
+import com.shibaura.tools.*;
+
+class Test04C{
+  public static void main(String[] args){
+    Car myCar = new Car();
+    myCar.setSpeed(100.0);
+    System.out.println("velocity:" + myCar.getSpeed());
+  }
+}
