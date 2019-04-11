519a46cc26032df2f7f849585b33e7d53191cc0f
Doppon <zaxpkuraudo7777@gmail.com>
2019-04-11 11:49:56 +0900

    意図的にエラーを吐き出すSaifuプログラムを作成

---
+++ 2019/Lesson2/src/Saifu.java
@@ -0,0 +1,23 @@
+class HelloWorld {
+	/*
+	public static void main(String[] args) {
+		
+	}
+	*/
+	public int okane = 1000;
+
+	public void setOkane(money) {
+		okane = money;
+	}
+
+	public void getOkane() {
+		return okane;
+	}
+}
+
+/*
+Saifu.java:9: エラー: <identifier>がありません
+        public void setOkane(money) {
+                                  ^
+エラー1個
+*/
