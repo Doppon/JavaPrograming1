69dfb052cb9adc135ada99077b4c57b8124244ab
Doppon <zaxpkuraudo7777@gmail.com>
2019-04-11 12:09:01 +0900

    第2回(第1回演習)の発展問題1を追加

---
+++ 2019/Lesson2/src/Dev1.java
@@ -0,0 +1,9 @@
+class Dev1 {
+    public static void main(String[] args) {
+        if (args.length >= 2) {
+            System.out.println("引数はひとつだけです。");
+        } else {
+            System.out.println(args[0]);
+        }
+    }
+}
