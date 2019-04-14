66e6990cd5c1d2332a9edc452e008f339dea20fd
Doppon <zaxpkuraudo7777@gmail.com>
2019-04-14 23:58:02 +0900

    インテンドのリファクタリング

--- 2018/Lesson2/src/Saifu.java
+++ 2018/Lesson2/src/Saifu.java
@@ -1,12 +1,12 @@
 // このプログラムは意図的に間違ったプログラムを書いています。
 class Saifu {
-		public int okane = 1000;
+	public int okane = 1000;
 
-		public void setOkane(int yen) {
-				okane = yen;
-		}
+	public void setOkane(int yen) {
+		okane = yen;
+	}
 
-		public int getOkane() {
-				return okane;
-		}
+	public int getOkane() {
+		return okane;
+	}
 }
