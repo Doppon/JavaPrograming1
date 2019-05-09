3263b2da866058ea6087e58055204ea4012912b6
Doppon <zaxpkuraudo7777@gmail.com>
2019-05-09 12:16:05 +0900

    クラスの設定を変更

--- 2019/Lesson7/src/Bank.java
+++ 2019/Lesson7/src/Bank.java
@@ -1,37 +1,41 @@
-class Bank {
+public class Bank {
+
+
+    private static class BankAccount {
+        int okane = 0;
+        String bank = "TokyoBank";
+    
+        void addOkane(int i) {
+            okane += 1;
+        }
+    
+        void print() {
+            System.out.println(bank + okane + "yen");
+        }
+    }
+    
+    
+    private static class ChokinBako {
+        int okane = 0;
+        boolean available = true;
+    
+        void addOkane(int i) {
+            okane += 1;
+        }
+    
+        void print() {
+            System.out.println(bank + okane + "yen");
+        }
+    
+        boolean getAvailability() {
+            return available;
+        }
+    }
+    
+
     public static void main(String[] args) {
         System.out.println("hoge");
     }
 }
 
 
-class BankAccount {
-    int okane = 0;
-    String bank = "TokyoBank";
-
-    void addOkane(int i) {
-        okane += 1;
-    }
-
-    void print() {
-        System.out.println(bank + okane + "yen");
-    }
-}
-
-
-class ChokinBako {
-    int okane = 0;
-    boolean available = true;
-
-    void addOkane(int i) {
-        okane += 1;
-    }
-
-    void print() {
-        System.out.println(bank + okane + "yen");
-    }
-
-    boolean getAvailability() {
-        return available;
-    }
-}
