ffcbaf7c769e5cc23ac7fa61227a559e56eb65b1
Doppon <zaxpkuraudo7777@gmail.com>
2019-05-09 11:55:42 +0900

    mainの処理を追加

--- 2019/Lesson7/src/Bank.java
+++ 2019/Lesson7/src/Bank.java
@@ -1,3 +1,10 @@
+class Bank {
+    public static void main(String[] args) {
+        System.out.println("hoge");
+    }
+}
+
+
 class BankAccount {
     int okane = 0;
     String bank = "TokyoBank";
