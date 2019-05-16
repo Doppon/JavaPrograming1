a11a820234c3ae96921bc59afa3ad243506c2a3e
Doppon <zaxpkuraudo7777@gmail.com>
2019-05-16 11:33:57 +0900

    05Bのサンプルテキスト読み込みのプログラムを追加

---
+++ 2019/Lesson10/src/Test05B.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class Test05B {
+    public static void main(String[] args) {
+        BufferedReader bf = null;
+        String strData = "";
+        try{ 
+            FileInputStream fI = new FileInputStream("Sample.txt");
+            InputStreamReader iS = new InputStreamReader(fI,"SJIS");
+            bf=new BufferedReader(iS);
+            while((strData=bf.readLine())!=null) {
+                System.out.println(strData);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
