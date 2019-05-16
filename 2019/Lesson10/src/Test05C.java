e0937db455cbacd86f1e52e2cc06af36d7700840
Doppon <zaxpkuraudo7777@gmail.com>
2019-05-16 11:15:06 +0900

    引数を受け取る部分でNumberFormatExceptionの例外処理を設定

--- 2019/Lesson10/src/Test05C.java
+++ 2019/Lesson10/src/Test05C.java
@@ -1,11 +1,15 @@
 class Test05C {
     public static void main(String[] args) {
-        int count = Integer.parseInt(args[0]);
-        int[] intArray = new int[count];
-
-        for(int j=0; j<intArray.length; j++) {
-            intArray[j] = (int)(Math.random() * 10) + 1;
-            System.out.println(intArray[j]);
+        try {
+            int count = Integer.parseInt(args[0]);
+            int[] intArray = new int[count];
+    
+            for(int j=0; j<intArray.length; j++) {
+                intArray[j] = (int)(Math.random() * 10) + 1;
+                System.out.println(intArray[j]);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("数字以外の文字が入力されました");
         }
     }
 }
