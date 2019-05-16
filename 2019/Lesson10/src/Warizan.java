5dd4a3cb767fb28eaaec4ae5a121adee0b5481cd
Doppon <zaxpkuraudo7777@gmail.com>
2019-05-16 09:40:20 +0900

    資料に載せられているWarizanクラスを実装

---
+++ 2019/Lesson10/src/Warizan.java
@@ -0,0 +1,14 @@
+class Warizan {
+    int a = 0;
+    int b = 0;
+    void setA(int a) {
+        this.a = a;
+    }
+    void setB(int b) {
+        this.b = b;
+    }
+    int div() {
+        int r = this.a / this.b;
+        return r;
+    }
+}
