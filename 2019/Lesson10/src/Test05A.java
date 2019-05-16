5ab317c684a223c71b0822249f594f3fe4a63abe
Doppon <zaxpkuraudo7777@gmail.com>
2019-05-16 09:47:25 +0900

    もし0で割ろうとしたときにArithmeticExceptionのエラーが発生する様子をコメントで追加

--- 2019/Lesson10/src/Test05A.java
+++ 2019/Lesson10/src/Test05A.java
@@ -3,6 +3,13 @@ class Test05A {
         Warizan w = new Warizan();
         w.setA(10);
         w.setB(3);
+        /*
+        w.setB(3); -> w.setB(0);
+
+        Exception in thread "main" java.lang.ArithmeticException: / by zero
+        at Warizan.div(Warizan.java:11)
+        at Test05A.main(Test05A.java:6)
+        */
         int result = w.div();
         System.out.println("result=" + result);
     }
