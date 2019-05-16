502e98586b1ea04bef07141af42c38775b71b2b5
Doppon <zaxpkuraudo7777@gmail.com>
2019-05-16 09:44:42 +0900

    10回の資料に載っているTest05のサンプルコードを追加

---
+++ 2019/Lesson10/src/Test05A.java
@@ -0,0 +1,9 @@
+class Test05A {
+    public static void main(String[] args) {
+        Warizan w = new Warizan();
+        w.setA(10);
+        w.setB(3);
+        int result = w.div();
+        System.out.println("result=" + result);
+    }
+}
