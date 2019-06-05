6bd818cbf60839eee1350af3fb73015968ef2410
Doppon <zaxpkuraudo7777@gmail.com>
2019-06-05 20:19:05 +0900

    Calculatorクラスに必要な3つのデータを初期化

---
+++ 2019/Lesson14/src/Calculator.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+class Calculator{
+    ArrayList<Integer> dataList = new ArrayList<Integer>();
+    ArrayList<CalcNode> nodeList = new ArrayList<CalcNode>();
+    long sum;
+}
